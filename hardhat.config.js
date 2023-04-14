6abee57827e2d9a5e27de86b758e8a7324ceea5d
dankorotenko <danilkorotenko2@gmail.com>
2023-04-14 19:57:13 +0200

    smart contract for crowdfunding platform

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,18 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: {
-    version: '0.8.9',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+export const solidity = {
+  version: "0.8.9",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/YMSCWozsGGK1k2_ORyGRS7t2W_kKRaE3`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
     },
   },
 };
